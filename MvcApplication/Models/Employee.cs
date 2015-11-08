03934abed4d3466d7ff3d243fe2fe7ae737be2a5
mynameiscaoyang <280916494@qq.com>
2015-11-09 01:30:48 +0800

    test

---
+++ MvcApplication/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication.Models
+{
+    public class Employee
+    {
+        public string userName { get; set; }
+        public int age { get; set; }
+    }
+
+}
