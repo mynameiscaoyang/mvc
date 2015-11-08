7cb70d5f76ff0610a79ea729d72bd16f64c95ea9
mynameiscaoyang <280916494@qq.com>
2015-11-09 01:28:10 +0800

    test

---
+++ MvcApplication.Tests/Test/MyTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvcApplication.Tests.Test
+{
+    [TestClass]
+    public class MyTest
+    {
+        [TestMethod]
+        public void testString()
+        {
+            String temp = "caoyang";
+            Assert.AreEqual("caoyang", temp);
+        }
+    }
+}
